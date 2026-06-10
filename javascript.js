1e3bdbd1a0069aa61f72c42a2f3cef2c79bc2a4b
rindhya rani <rindhyaranisenthilkumar@gmail.com>
2026-06-10 15:05:55 +0530

    JS

---
+++ javascript.js
@@ -0,0 +1,31 @@
+console.log("Print statement")
+const a=20
+const b=30
+console.log(a+b)
+const str="Rindhya"
+console.log(str)
+const arr=[1,2.4,"String",false]
+console.log(arr)
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i])
+}
+arr[1]=3.1
+console.log(arr[1])
+arr.push("Rani")
+console.log(arr)
+arr.pop()
+console.log(arr)
+function example(){
+    console.log("I am writing something")
+}
+const ptag=document.getElementById("ptag")
+ptag.innerHTML="Print Statement"
+const input=document.getElementById("input")
+const output=document.getElementById("inputField")
+input.addEventListener("input",()=>{
+    output.textContent=input.value
+})
+const first=document.createElement("h1")
+first.textContent="Random text"
+document.body.appendChild(first)
+first.remove()
